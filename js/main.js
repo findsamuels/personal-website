140df25ad38b6c0199300e975cafc64ab19b3a4f
Samuels125 <samuels125@outlook.com>
2019-03-17 20:29:10 +0000

    commit 1

--- js/main.js
+++ js/main.js
@@ -1,8 +1,32 @@
 
+// alert("Loaded!");
+
+
+var items = document.querySelector('.nav__item');
+var toggler = document.querySelector('.nav__toggle');
 
 
 
-function alert() {
-    alert("Loaded!");
-}
-alert();
+
+    toggler.addEventListener("click", function(){
+        alert("Loaded!");
+        if (items.style.display === "none") {
+            items.style.display = "block";
+            items.style.opacity = "1";
+
+        }
+        else {
+            items.style.display = "block";
+        }
+    });
+        
+        
+
+
+   
+
+
+
+
+
+
