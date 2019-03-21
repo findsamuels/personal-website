a479c936c532febd7808b61e9062d947fb7f0936
Samuels125 <samuels125@outlook.com>
2019-03-21 13:38:25 +0000

    commit 1

--- js/main.js
+++ js/main.js
@@ -9,12 +9,7 @@ var toggler = document.querySelector('.nav__togglecontainer');
 
 var isDisplay;
 
-// var show = function(items){
-//     items.classList.add("is-visible");
-// }
-// var hide = function (items) {
-//     items.classList.remove("is-visible");
-// }
+
 
 if ((window.innerWidth > 800) && (window.getComputedStyle(items).display === "none") || (window.getComputedStyle(items).display === "block")) {
 
