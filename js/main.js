385ea672f2e5c3babc6564541e9cf0779ff2bc82
Samuels125 <samuels125@outlook.com>
2019-03-21 11:37:51 +0000

    commit 1

--- js/main.js
+++ js/main.js
@@ -3,22 +3,34 @@
 
 
 var items = document.querySelector('.nav__item');
-var toggler = document.querySelector('.nav__toggle');
+
+var toggler = document.querySelector('.nav__togglecontainer');
 
 
+var isDisplay;
 
+// var show = function(items){
+//     items.classList.add("is-visible");
+// }
+// var hide = function (items) {
+//     items.classList.remove("is-visible");
+// }
+
+if ((window.innerWidth > 800) && (window.getComputedStyle(items).display === "none") || (window.getComputedStyle(items).display === "block")) {
+
+    items.style.display = "none";
+
+
+}
 
     toggler.addEventListener("click", function(){
-        alert("Loaded!");
-        if (items.style.display === "none") {
-            items.style.display = "block";
-            items.style.opacity = "1";
 
-        }
-        else {
-            items.style.display = "block";
-        }
+        items.classList.toggle("is-visible");
+        
+       
+        
     });
+
         
         
 
