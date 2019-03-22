847f6b6288e66d5c0c90e71db5129388b54ecf8e
Samuels125 <samuels125@outlook.com>
2019-03-22 11:27:37 +0000

    commit 1

--- js/main.js
+++ js/main.js
@@ -11,13 +11,6 @@ var isDisplay;
 
 
 
-if ((window.innerWidth > 800) && (window.getComputedStyle(items).display === "none") || (window.getComputedStyle(items).display === "block")) {
-
-    items.style.display = "none";
-
-
-}
-
     toggler.addEventListener("click", function(){
 
         items.classList.toggle("is-visible");
