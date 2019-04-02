cc58fafb959bc5cf88522f23bd2df7e252ad19bd
Samuels125 <samuels125@outlook.com>
2019-04-02 23:57:40 +0100

    commit 1

--- js/main.js
+++ js/main.js
@@ -10,16 +10,19 @@ var toggler = document.querySelector('.nav__togglecontainer');
 
 var allTab = document.querySelector('#all');
 var funTab = document.querySelector('#fun');
-var communityTab = document.querySelector('#business');
-var businessTab = document.querySelector('#comminity');
+var communityTab = document.querySelector('#community');
+var businessTab = document.querySelector('#business');
 
 var funItem = document.querySelectorAll('#fun-item');
 var allItem = document.querySelectorAll('#all-item');
-var communityItem = document.querySelectorAll('#business-item');
-var businessItem = document.querySelectorAll('#comminity-item');
+var communityItem = document.querySelectorAll('#community-item');
+var businessItem = document.querySelectorAll('#business-item');
 
 var tabItems = document.querySelectorAll('.tabs-col-4__items');
-    hideTabItems();
+
+var tabHead = document.querySelectorAll('.tabs-col-4__head');
+var tabHeadText = document.querySelectorAll('tabs-col-4__head__headingStyle');
+    
 
 
 var isDisplay;
@@ -28,19 +31,82 @@ var isDisplay;
     });
 
 
-funTab.addEventListener('click', function(){
-    for (var i = 0; i < funItem.length; i++) {
-        funItem[i].style.display = 'block';
+// for (var i = 0; i < tabHead.length; i++) {
+//     var tabHeader = tabHead[i];
+//     tabHeader.addEventListener('click', function() {
 
-    }
-});
+//         tabHeader.classList.toggle('wineBack');
+//     });
 
-allTab.addEventListener('click', function(){
-    for (var i = 0; i < tabItems.length; i++) {
-        tabItems[i].style.display = 'block';
+    
+// }
 
-    }
-});
+
+
+tabToggle();
+
+   function tabToggle(){
+
+       
+
+
+       funTab.addEventListener('click', function () {
+           for (var i = 0; i < funItem.length; i++) {
+
+               hideTabItems();
+               funTab.classList.add('wineBack');
+               funItem[i].style.display = 'block';
+               
+
+               allTab.classList.remove('wineBack');
+             
+               communityTab.classList.remove('wineBack'); 
+               businessTab.classList.remove('wineBack'); 
+
+           }
+       });
+
+       businessTab.addEventListener('click', function () {
+           for (var i = 0; i < businessItem.length; i++) {
+
+               hideTabItems();
+               businessItem[i].style.display = 'block';
+
+               businessTab.classList.add('wineBack'); 
+               allTab.classList.remove('wineBack');
+               communityTab.classList.remove('wineBack');
+               funTab.classList.remove('wineBack'); 
+
+
+           }
+       });
+       communityTab.addEventListener('click', function () {
+           for (var i = 0; i < communityItem.length; i++) {
+
+               hideTabItems();
+               communityItem[i].style.display = 'block';
+
+               communityTab.classList.add('wineBack');
+               businessTab.classList.remove('wineBack');
+               allTab.classList.remove('wineBack');
+               funTab.classList.remove('wineBack'); 
+           }
+       });
+
+       allTab.addEventListener('click', function () {
+           for (var i = 0; i < tabItems.length; i++) {
+               tabItems[i].style.display = 'block';
+
+               allTab.classList.add('wineBack');
+               communityTab.classList.remove('wineBack');
+               businessTab.classList.remove('wineBack');
+               funTab.classList.remove('wineBack');
+
+           }
+       });
+   }
+
+
 
    
 function hideTabItems(){
