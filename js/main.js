029c16a0ec511d4b4cac9bebfa7fde0e53396f14
Samuels125 <samuels125@outlook.com>
2019-04-04 13:58:18 +0100

    commit 1

--- js/main.js
+++ js/main.js
@@ -13,12 +13,12 @@ var funTab = document.querySelector('#fun');
 var communityTab = document.querySelector('#community');
 var businessTab = document.querySelector('#business');
 
-var funItem = document.querySelectorAll('#fun-item');
-var allItem = document.querySelectorAll('#all-item');
-var communityItem = document.querySelectorAll('#community-item');
-var businessItem = document.querySelectorAll('#business-item');
+var funItem = document.querySelectorAll('.fun-content');
 
-var tabItems = document.querySelectorAll('.tabs-col-4__items');
+var communityItem = document.querySelectorAll('.community-content');
+var businessItem = document.querySelectorAll('.business-content');
+
+var tabItems = document.querySelectorAll('.card-1');
 
 var tabHead = document.querySelectorAll('.tabs-col-4__head');
 var tabHeadText = document.querySelectorAll('tabs-col-4__head__headingStyle');
@@ -53,15 +53,16 @@ tabToggle();
        funTab.addEventListener('click', function () {
            for (var i = 0; i < funItem.length; i++) {
 
-               hideTabItems();
+               
+               hideBusinessItems();
+               hideCommunityItems();
+             
                funTab.classList.add('wineBack');
                funItem[i].style.display = 'block';
-               
-
                allTab.classList.remove('wineBack');
              
-               communityTab.classList.remove('wineBack'); 
-               businessTab.classList.remove('wineBack'); 
+               
+               
 
            }
        });
@@ -69,13 +70,13 @@ tabToggle();
        businessTab.addEventListener('click', function () {
            for (var i = 0; i < businessItem.length; i++) {
 
-               hideTabItems();
+               
+               hideFunItems();
+               hideCommunityItems();
+               hideAllTabColor();
                businessItem[i].style.display = 'block';
-
                businessTab.classList.add('wineBack'); 
-               allTab.classList.remove('wineBack');
-               communityTab.classList.remove('wineBack');
-               funTab.classList.remove('wineBack'); 
+               
 
 
            }
@@ -83,13 +84,13 @@ tabToggle();
        communityTab.addEventListener('click', function () {
            for (var i = 0; i < communityItem.length; i++) {
 
-               hideTabItems();
+               
+               hideFunItems();
+             hideBusinessItems();
+               hideAllTabColor();
                communityItem[i].style.display = 'block';
-
                communityTab.classList.add('wineBack');
-               businessTab.classList.remove('wineBack');
-               allTab.classList.remove('wineBack');
-               funTab.classList.remove('wineBack'); 
+                
            }
        });
 
@@ -98,21 +99,35 @@ tabToggle();
                tabItems[i].style.display = 'block';
 
                allTab.classList.add('wineBack');
-               communityTab.classList.remove('wineBack');
-               businessTab.classList.remove('wineBack');
+               businessTab.classList.remove('wineBack'); 
                funTab.classList.remove('wineBack');
-
+               communityTab.classList.remove('wineBack'); 
            }
        });
    }
 
+   ///////////////////FUNCTIONS DECLARATIONS
+   function hideAllTabColor(){
+       allTab.classList.remove('wineBack');
+   }
 
+function hideBusinessItems() {
+    for (var i = 0; i < businessItem.length; i++) {
+        businessItem[i].style.display = 'none';
+        businessTab.classList.remove('wineBack'); 
+    }
+}
+function hideFunItems() {
+    for (var i = 0; i < funItem.length; i++) {
+        funItem[i].style.display = 'none';
+        funTab.classList.remove('wineBack');
 
-   
-function hideTabItems(){
-    for (var i = 0; i < tabItems.length; i++) {
-        tabItems[i].style.display = 'none';
-
+    }
+}
+function hideCommunityItems() {
+    for (var i = 0; i < communityItem.length; i++) {
+        communityItem[i].style.display = 'none';
+        communityTab.classList.remove('wineBack'); 
     }
 }
         
