15b8a649051825ef6a227c71e986cccdcdc1f806
Samuels125 <samuels125@outlook.com>
2019-04-10 14:15:35 +0100

    commit 1

--- js/main.js
+++ js/main.js
@@ -3,155 +3,189 @@
 
 
 
-//  init();
-
-var items = document.querySelector('.nav__item');
-var toggler = document.querySelector('.nav__togglecontainer');
-
-var allTab = document.querySelector('#all');
-var funTab = document.querySelector('#fun');
-var communityTab = document.querySelector('#community');
-var businessTab = document.querySelector('#business');
-
-var funItem = document.querySelectorAll('.fun-content');
-
-var communityItem = document.querySelectorAll('.community-content');
-var businessItem = document.querySelectorAll('.business-content');
-
-var tabItems = document.querySelectorAll('.card-1');
-
-var tabHead = document.querySelectorAll('.tabs-col-4__head');
-var tabHeadText = document.querySelectorAll('tabs-col-4__head__headingStyle');
-    
 
 
-var isDisplay;
-    toggler.addEventListener("click", function(){
-        items.classList.toggle("is-visible");
-    });
 
-
-// for (var i = 0; i < tabHead.length; i++) {
-//     var tabHeader = tabHead[i];
-//     tabHeader.addEventListener('click', function() {
-
-//         tabHeader.classList.toggle('wineBack');
-//     });
 
     
-// }
-
-
-
-
-
-       
-    function clickedTab(tabName){
-
-            return function (item) {
-                tabName.addEventListener('click', function () {
-                switch (true) {
-                    case tabName === funTab:
-                        for (var i = 0; i < item.length; i++) {
-                        hideBusinessItems();
-                        hideCommunityItems();
-                        funTab.classList.add('wineBack');
-                        item[i].style.display = 'block';
-                        allTab.classList.remove('wineBack');
-                        }
-                        break;
-
-                    case tabName === businessTab:
-                        for (var i = 0; i < item.length; i++) {
-                        hideFunItems();
-                        hideCommunityItems();
-                        hideAllTabColor();
-                        item[i].style.display = 'block';
-                        businessTab.classList.add('wineBack');
-                        }
-                        break;
-
-                    case tabName === communityTab:
-                        for (var i = 0; i < item.length; i++) {
-                        hideFunItems();
-                        hideBusinessItems();
-                        hideAllTabColor();
-                        item[i].style.display = 'block';
-                        communityTab.classList.add('wineBack');
-                        }
-                        break;
-
-                    case tabName === allTab:
-                        for (var i = 0; i < item.length; i++) {
-                        item[i].style.display = 'block';
-
-                        allTab.classList.add('wineBack');
-                        businessTab.classList.remove('wineBack');
-                        funTab.classList.remove('wineBack');
-                        communityTab.classList.remove('wineBack');
-                        }
-                        break;
-                        default:
-                        console.log('Error');
-
-                }
-                });
-            }
-        
-
-        
-    }
-
-    clickedTab(funTab)(funItem);
-clickedTab(businessTab)(businessItem);
-       clickedTab(communityTab)(communityItem);
-clickedTab(allTab)(tabItems);
-
-
-   
-   
-
-   ///////////////////FUNCTIONS DECLARATIONS
-   function hideAllTabColor(){
-       allTab.classList.remove('wineBack');
-   }
-
-function hideBusinessItems() {
-    for (var i = 0; i < businessItem.length; i++) {
-        businessItem[i].style.display = 'none';
-        businessTab.classList.remove('wineBack'); 
-    }
-}
-function hideFunItems() {
-    for (var i = 0; i < funItem.length; i++) {
-        funItem[i].style.display = 'none';
-        funTab.classList.remove('wineBack');
-
-    }
-}
-function hideCommunityItems() {
-    for (var i = 0; i < communityItem.length; i++) {
-        communityItem[i].style.display = 'none';
-        communityTab.classList.remove('wineBack'); 
-    }
-}
-        
-//Initialise
-// function init() {
-
-//     var items = document.querySelector('.nav__item');
-//     var toggler = document.querySelector('.nav__togglecontainer');
-
+  
 //     var allTab = document.querySelector('#all');
 //     var funTab = document.querySelector('#fun');
-//     var communityTab = document.querySelector('#business');
-//     var businessTab = document.querySelector('#comminity');
-//     var tabItems = document.querySelectorAll('.tabs-col-4__items');
+//     var communityTab = document.querySelector('#community');
+//     var businessTab = document.querySelector('#business');
 
-// }
+//     var funItem = document.querySelectorAll('.fun-content');
+
+//     var communityItem = document.querySelectorAll('.community-content');
+//     var businessItem = document.querySelectorAll('.business-content');
+
+//     var tabItems = document.querySelectorAll('.card-1');
+
+// var tabhead = document.querySelectorAll('.tabs-col-4__head');
+//     var tabHeadText = document.querySelectorAll('tabs-col-4__head__headingStyle');
 
 
-   
+
+
+        
+    
+
+ 
+        var AppUI  = (function(){
+////// Initialise App for UI
+    
+//////// Use initialised Dom for event
+           
+            var getDOM = {
+               toggler: '.nav__togglecontainer',
+                items: '.nav__item',
+                allTab: '#all',
+                funTab: '#fun',
+                communityTab: '#community',
+                businessTab: '#business',
+                funItem: '.fun-content',
+                communityItem: '.community-content',
+                businessItem: '.business-content',
+                tabItems: '.card-1',
+                tabHead: '.tabs-col-4__head',
+                tabHeadText: 'tabs-col-4__head__headingStyle',
+            }
+
+            return{
+                returnGetDom: function(){
+                    return getDOM;
+                   
+                }
+        
+            };
+
+        })();
+
+
+
+///App Controller or work calculation
+        var AppController = (function(){
+
+        })();
+
+/////////////////////////////////////////////////////////////////////////////////////
+        /////Controller
+        var Controller = (function(UIApp, ControllerApp){
+
+        var initialiseEvent = function(){
+            var handleUI = UIApp.returnGetDom();
+
+            var toggleNavBar = document.querySelector(handleUI.toggler);
+            var showNav = document.querySelector(handleUI.items);
+
+            var navfunTab = document.querySelector(handleUI.funTab);
+            var navbusinessTab = document.querySelector(handleUI.businessTab);
+            var navcommunityTab = document.querySelector(handleUI.communityTab);
+            var navallTab = document.querySelector(handleUI.allTab);
+
+            var navfunItem = document.querySelectorAll(handleUI.funItem);
+            var navbusinessItem = document.querySelectorAll(handleUI.businessItem);
+            var navcommunityItem = document.querySelectorAll(handleUI.communityItem);
+            var navallItem = document.querySelectorAll(handleUI.tabItems);
+
+//////////////////////////////////dom declaration
+           
+
+           
+            toggleNavBar.addEventListener("click", toggleNav);
+            navfunTab.addEventListener('click', toggleFunTab);
+            navbusinessTab.addEventListener('click', toggleBusinessTab);
+            navcommunityTab.addEventListener('click', toggleCommunityTab);
+            navallTab.addEventListener('click', toggleAllTab);
+                 
+//////////////////////////////////EVENT LISTENER
+            function toggleNav() {
+                showNav.classList.toggle("is-visible");
+                console.log('hello');
+            }
+                function toggleFunTab(){
+                    for (var i = 0; i < navfunItem.length; i++) {
+                        hideBusinessItems();
+                        hideCommunityItems();
+                        navfunTab.classList.add('wineBack');
+                        navfunItem[i].style.display = 'block';
+                        navallTab.classList.remove('wineBack');
+                    }
+                }
+            function toggleBusinessTab() {
+                for (var i = 0; i < navbusinessItem.length; i++) {
+                    hideFunItems();
+                    hideCommunityItems();
+                    hideAllTabColor();
+                    navbusinessItem[i].style.display = 'block';
+                    navbusinessTab.classList.add('wineBack');
+                }
+            }
+            function toggleCommunityTab() {
+                for (var i = 0; i < navcommunityItem.length; i++) {
+                    hideFunItems();
+                    hideBusinessItems();
+                    hideAllTabColor();
+                    navcommunityItem[i].style.display = 'block';
+                    navcommunityTab.classList.add('wineBack');
+                }
+            }
+            function toggleAllTab() {
+                for (var i = 0; i < navallItem.length; i++) {
+                    navallItem[i].style.display = 'block';
+
+                    navallTab.classList.add('wineBack');
+                    navbusinessTab.classList.remove('wineBack');
+                    navfunTab.classList.remove('wineBack');
+                    navcommunityTab.classList.remove('wineBack');
+                }
+            }
+            
+
+     
+            function hideAllTabColor() {
+        navallTab.classList.remove('wineBack');
+    }
+
+    function hideBusinessItems() {
+        for (var i = 0; i < navbusinessItem.length; i++) {
+            navbusinessItem[i].style.display = 'none';
+            navbusinessTab.classList.remove('wineBack');
+        }
+    }
+    function hideFunItems() {
+        for (var i = 0; i < navfunItem.length; i++) {
+            navfunItem[i].style.display = 'none';
+            navfunTab.classList.remove('wineBack');
+
+        }
+    }
+    function hideCommunityItems() {
+        for (var i = 0; i < navcommunityItem.length; i++) {
+            navcommunityItem[i].style.display = 'none';
+            navcommunityTab.classList.remove('wineBack');
+        }
+    }
+
+            
+
+
+
+
+        }
+            
+//////////////////////////RETURN DOM///////////////////////////////////////////////
+            return{
+                init: function(){
+                    initialiseEvent(); 
+                },
+                
+            }
+
+        })(AppUI, AppController);
+
+
+Controller.init();
 
 
 
